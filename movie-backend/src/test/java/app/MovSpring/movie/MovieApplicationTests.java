c80eb17cfe3f6a5c8ead721cebac7160d79b4691
jonathanjonn <135504914+jonathanjonn@users.noreply.github.com>
2023-12-02 16:12:39 +0700

    movie spring

---
+++ movie-backend/src/test/java/app/MovSpring/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package app.MovSpring.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
